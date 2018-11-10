b89d058e28b114a27bf8d3ae4701863ff129e465
Karina Vicente <karinavicente@node-ix2.pool-1-1.dynamic.totbb.net>
2018-11-09 18:21:09 -0800

    Initial Commit

---
+++ TravelingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelingApp
+//
+//  Created by Karina Vicente on 11/9/18.
+//  Copyright © 2018 Karina Vicente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
